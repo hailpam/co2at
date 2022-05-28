b0c9322df50af4ed9b0eec18e927813aa7932ed3
Paolo Maresca <plo.maresca@gmail.com>
2022-05-28 19:02:41 +0200

    Added certificate detail page (report QR to be done)

--- web/src/app/certificate/certificate.component.ts
+++ web/src/app/certificate/certificate.component.ts
@@ -1,3 +1,4 @@
+import { ThisReceiver } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 
 import { Router } from '@angular/router';
@@ -9,9 +10,44 @@ import { Router } from '@angular/router';
 })
 export class CertificateComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  certQrCodeInfo = '';
+  reportQrCodeInfo = '';
+
+  certificate = {
+    producer: '',
+    product: '',
+    provenance: '',
+    co2e_scope1: 0.0,
+    co2e_scope2: 0.0,
+    co2e_scope3: 0.0
+  };
+
+  constructor(private router: Router) {
+    const state = this.router.getCurrentNavigation()?.extras.state;
+    if (state !== undefined && state !== null) {
+      this.certificate = JSON.parse(JSON.stringify(state))
+      console.log(this.certificate);
+      this.certQrCodeInfo = this.certificate.product + this.certificate.producer + this.certificate.provenance + this.certificate.co2e_scope1 + this.certificate.co2e_scope2 + this.certificate.co2e_scope3;
+      // TODO link report if present, and prepare the QR code for the report
+      this.reportQrCodeInfo = 'report';
+    }
+  }
 
   ngOnInit(): void {
+    // single stats
+    this.singleStatScope1.data[0].value = this.certificate.co2e_scope1;
+    this.singleStatScope2.data[0].value = this.certificate.co2e_scope2;
+    this.singleStatScope3.data[0].value = this.certificate.co2e_scope3;
+
+    // network stats
+    this.parallelCoordinatesBreakdown.data[0].dimensions[0].values[0] = this.certificate.co2e_scope1;
+    this.parallelCoordinatesBreakdown.data[0].dimensions[1].values[0] = this.certificate.co2e_scope2;
+    this.parallelCoordinatesBreakdown.data[0].dimensions[2].values[0] = this.certificate.co2e_scope3;
+
+    // geolocation stats
+    const coe2e = this.certificate.co2e_scope1 + this.certificate.co2e_scope2 + this.certificate.co2e_scope3;
+    this.geoMapScopeTot.data[0].marker.size[0] = coe2e * 10;
+    this.geoMapScopeTot.data[0].marker.color[0] = coe2e * 10;
   }
 
   goToHome(): void {
@@ -21,4 +57,110 @@ export class CertificateComponent implements OnInit {
   goBack(): void {
     this.router.navigateByUrl("/certificates");
   }
+
+  public singleStatScope1 = {
+    data: [
+      {
+        type: "indicator",
+        mode: "number+gauge+delta",
+        gauge: { shape: "bullet" },
+        delta: { reference: 0.5 },
+        value: 1,
+        domain: { x: [0, 1], y: [0, 1] },
+        title: { text: "Scope1" }
+      }
+    ],
+    layout: { 
+      width: 400, height: 250 
+    }
+  };
+
+  public singleStatScope2 = {
+    data: [
+      {
+        type: "indicator",
+        mode: "number+gauge+delta",
+        gauge: { shape: "bullet" },
+        delta: { reference: 0.5 },
+        value: 1,
+        domain: { x: [0, 1], y: [0, 1] },
+        title: { text: "Scope2" }
+      }
+    ],
+    layout: { 
+      width: 400, height: 250 
+    }
+  };
+
+  public singleStatScope3 = {
+    data: [
+      {
+        type: "indicator",
+        mode: "number+gauge+delta",
+        gauge: { shape: "bullet" },
+        delta: { reference: 0.5 },
+        value: 1,
+        domain: { x: [0, 1], y: [0, 1] },
+        title: { text: "Scope3" }
+      }
+    ],
+    layout: { 
+      width: 400, height: 250 
+    }
+  };
+
+  public parallelCoordinatesBreakdown = {
+    data: [{
+      type: 'parcoords',
+      line: {
+        color: 'green'
+      },
+      dimensions: [{
+        range: [0, 2],
+        label: 'Scope1',
+        values: [ 0.2 ]
+      }, {
+        range: [0, 2],
+        label: 'Scope2',
+        values: [ 1.1 ]
+      }, {
+        range: [0, 2],
+        label: 'Scope3',
+        values: [ 0.7 ]
+      }]
+    }],
+    layout: {
+      width: 600
+    }
+  };
+
+  public geoMapScopeTot = {
+    data: [{
+      type: 'scattergeo',
+      mode: 'markers',
+      locations: [ 'USA' ],
+      marker: {
+        size: [20],
+        color: [20],
+        cmin: 0,
+        cmax: 50,
+        colorscale: 'Reds',
+        colorbar: {
+          title: 'CO2e',
+          ticksuffix: '%',
+          showticksuffix: 'last'
+        },
+        line: {
+          color: 'black'
+        }
+      },
+      name: 'United States Data'
+    }],
+    layout: {
+      'geo': {
+        'scope': 'usa',
+        'resolution': 50
+      }
+    }
+  };
 }
--- web/src/app/report/report.component.ts
+++ web/src/app/report/report.component.ts
@@ -21,4 +21,85 @@ export class ReportComponent implements OnInit {
   goBack(): void {
     this.router.navigateByUrl("/reports");
   }
+
+  public singleStatScope1 = {
+    data: [
+      {
+        type: "indicator",
+        mode: "number+gauge+delta",
+        gauge: { shape: "bullet" },
+        delta: { reference: 300 },
+        value: 220,
+        domain: { x: [0, 1], y: [0, 1] },
+        title: { text: "Profit" }
+      }
+    ],
+    layout: {
+      width: 400, height: 250
+    }
+  };
+
+  public geoMapScopeTot = {
+    data: [{
+      type: 'scattergeo',
+      mode: 'markers',
+      locations: ['FRA', 'DEU', 'RUS', 'ESP', 'USA'],
+      marker: {
+        size: [20, 30, 15, 10, 100],
+        color: [10, 20, 40, 50, 100],
+        cmin: 0,
+        cmax: 50,
+        colorscale: 'Greens',
+        colorbar: {
+          title: 'Some rate',
+          ticksuffix: '%',
+          showticksuffix: 'last'
+        },
+        line: {
+          color: 'black'
+        }
+      },
+      name: 'europe data'
+    }],
+    layout: {
+      'geo': {
+        'scope': 'europe',
+        'resolution': 50
+      }
+    }
+  }
+
+  public parallelCoordinatesBreakdown = {
+    data: [{
+      type: 'parcoords',
+      line: {
+        color: 'blue'
+      },
+
+      dimensions: [{
+        range: [1, 5],
+        constraintrange: [1, 2],
+        label: 'A',
+        values: [1, 4]
+      }, {
+        range: [1, 5],
+        label: 'B',
+        values: [3, 1.5],
+        tickvals: [1.5, 3, 4.5]
+      }, {
+        range: [1, 5],
+        label: 'C',
+        values: [2, 4],
+        tickvals: [1, 2, 4, 5],
+        ticktext: ['text 1', 'text 2', 'text 4', 'text 5']
+      }, {
+        range: [1, 5],
+        label: 'D',
+        values: [4, 2]
+      }]
+    }],
+    layout: {
+      width: 800
+    }
+  };
 }
